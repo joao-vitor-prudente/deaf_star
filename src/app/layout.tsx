f5ac2039d779cd215916085f428c9c0430166cea
João Vitor Ceolin Prudente <joao.ceolin@origemmotos.com.br>
2024-12-08 19:09:54 -0300

    feat: add client side session access

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { type ReactNode } from "react";
-import { TRPCReactProvider } from "~/trpc/react";
 import { ModeToggle } from "~/components/ui/mode-toggle";
+import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
   title: "Deaf Star",
@@ -20,19 +21,21 @@ export default function RootLayout(props: RootLayoutProps): React.JSX.Element {
   return (
     <html lang="pt-BR" className={GeistSans.variable} suppressHydrationWarning>
       <body>
-        <TRPCReactProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <aside className="absolute top-4 right-4">
-              <ModeToggle />
-            </aside>
-            {props.children}
-          </ThemeProvider>
-        </TRPCReactProvider>
+        <SessionProvider>
+          <TRPCReactProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <aside className="absolute right-4 top-4">
+                <ModeToggle />
+              </aside>
+              {props.children}
+            </ThemeProvider>
+          </TRPCReactProvider>
+        </SessionProvider>
       </body>
     </html>
   );
